672087922ef22d93019d0b0a2f3c6f2c61ac7dc4
NofalTechuz <nofal@techuz.com>
2024-07-23 12:38:16 +0530

    auth middleware chnages

--- react-basecode/src/pages/Admin/Dashboard/Home.js
+++ react-basecode/src/pages/Admin/Dashboard/Home.js
@@ -1,6 +1,23 @@
 import React from 'react'
 
 const Home = () => {
+
+  useEffect(() => {
+
+    const getUserData = async () => {
+      try {
+        const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/user`, {
+          withCredentials: true,
+        });
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getUserData();
+
+  }, [])
+  
   return (
     <div>Home</div>
   )
