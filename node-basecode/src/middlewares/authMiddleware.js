6e1f75d2c2a0e65e8e52a74ef38af5dd2cd1b0fa
Savan Patel <savan@techuz.com>
2024-07-25 11:21:07 +0530

    updated some auth middlewares error handling

--- node-basecode/src/middlewares/authMiddleware.js
+++ node-basecode/src/middlewares/authMiddleware.js
@@ -1,21 +1,22 @@
 // middlewares/authMiddleware.js
-const jwt = require('jsonwebtoken');
-const config = require('../config/config');
-const cookieParser = require('cookie-parser');
+const jwt = require("jsonwebtoken");
+const config = require("../config/config");
+const cookieParser = require("cookie-parser");
+const AppError = require("../utils/appError");
 
 const authMiddleware = (req, res, next) => {
   const authHeader = req.headers.authorization;
 
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return res.status(401).json({ msg: 'No token provided' });
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return next(new AppError("No token provided", 401));
   }
 
   // Extract the token
-  const token = authHeader.split(' ')[1];
+  const token = authHeader.split(" ")[1];
 
   // Verify the token
   jwt.verify(token, config.jwtSecret, (err, user) => {
-    if (err) return res.status(403).json({ msg: 'Invalid token' });
+    if (err) return next(new AppError("Invalid token", 403));
 
     req.user = user; // Attach the user info to the request
     next();
